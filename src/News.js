314f4c5224cc653a7156c7f3fa32ea73c9c23c3e
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-11-13 11:35:45 +0100

    Update News.js

--- src/News.js
+++ src/News.js
@@ -1,86 +1,87 @@
-import React,{useState,useRef,useContext,useEffect} from 'react'
-import { Link} from 'react-router-dom';
+import React, { useState, useRef, useContext, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 import './news.css'
 import Footer from './Footer'
-import {contextWrapper} from './App'
+import { contextWrapper } from './App'
 const News = () => {
-    let data=useContext(contextWrapper)
-    const [country,setcountry,countryFlag, setcountryFlag]=data
+    let data = useContext(contextWrapper)
+    const [country, setcountry, countryFlag, setcountryFlag] = data
     const [result, setresult] = useState([])
     const [noResult, setnoResult] = useState(false)
     const [searching, setsearching] = useState(true)
     const [loadMore, setloadMore] = useState(false)
     const [pagination, setpagination] = useState(false)
     const [page, setpage] = useState(0)
-    let menu=useRef()
-    const Menu=()=>{
+    let menu = useRef()
+    const Menu = () => {
         menu.current.classList.toggle('hamburgermenu')
     }
-   const handleSearch=(e)=>{
-    setnoResult(false)
-    setsearching(true)
-    fetch(`https://newsdata.io/api/1/news?apikey=pub_210660b996e06920b7f144ea9530f2b020ef9&country=${country}`)
-        .then(res=>res.json())
-        .then(res=>{
-            if(res?.status !== 'success' ){
-                alert(res?.message)
-            }
-            if(res?.results?.length===0){
-                setsearching(false)
-                setnoResult(true)
-            }else{
-                setsearching(false)
-                res?.results.map(item=>{
-                    setresult((a)=>[...a,item])
-                  })    
-                setpage(res.nextPage)
-                setpagination(true)
-            }
-          
-        })
-        .catch(err=>console.log(err))
+    const handleSearch = (e) => {
+        setnoResult(false)
+        setsearching(true)
+        fetch(`https://newsdata.io/api/1/news?apikey=pub_210660b996e06920b7f144ea9530f2b020ef9&country=${country}`)
+            .then(res => res.json())
+            .then(res => {
+                if (res?.status !== 'success') {
+                    alert(res?.message)
+                }
+                if (res?.results?.length === 0) {
+                    setsearching(false)
+                    setnoResult(true)
+                } else {
+                    setsearching(false)
+                    res?.results.map(item => {
+                        setresult((a) => [...a, item])
+                    })
+                    setpage(res.nextPage)
+                    setpagination(true)
+                }
+
+            })
+            .catch(err => console.log(err))
     }
 
-    const LoadMore=(e)=>{
+    const LoadMore = (e) => {
         setnoResult(false)
         setloadMore(true)
         fetch(`https://newsdata.io/api/1/news?apikey=pub_210660b996e06920b7f144ea9530f2b020ef9&country=${country}&page=${page}`)
-            .then(res=>res.json())
-            .then(res=>{
-                if(res?.status !== 'success' ){
+            .then(res => res.json())
+            .then(res => {
+                if (res?.status !== 'success') {
                     alert(res?.message)
                 }
-                if(res?.results?.length===0){
+                if (res?.results?.length === 0) {
                     setloadMore(false)
                     setnoResult(true)
-                }else{
+                } else {
                     setloadMore(false)
-                    res?.results.map(item=>{
-                        setresult((a)=>[...a,item])
-                      })    
+                    res?.results.map(item => {
+                        setresult((a) => [...a, item])
+                    })
                     setpage(res.nextPage)
                     setpagination(true)
                 }
             })
-            .catch(err=>console.log(err))
-        }
+            .catch(err => console.log(err))
+    }
 
-    useEffect(()=>{
+    useEffect(() => {
         setresult([])
         setpagination(false)
         handleSearch()
-    },[country])
-    const changeImg=(dt)=>{
-        setcountryFlag(dt.target.currentSrc.split('com')[1])
+    }, [country])
+    const changeImg = (dt) => {
+        console.log(dt)
+        setcountryFlag(dt.target.currentSrc.split('app')[1])
     }
-  return (
-    <div className="News">
-        <nav>
-            <div className="h1">
-                <h1 onClick={()=>handleSearch()}>News</h1>
-                <h1 style={{color:'blue'}}>HUB</h1>
-            </div>
+    return (
+        <div className="News">
+            <nav>
+                <div className="h1">
+                    <h1 onClick={() => handleSearch()}>News</h1>
+                    <h1 style={{ color: 'blue' }}>HUB</h1>
+                </div>
                 <ul ref={menu} >
                     <li><Link to='/business'>Business</Link></li>
                     <li><Link to='/entertainment'>Entertainment</Link></li>
@@ -93,60 +94,60 @@ const News = () => {
                 <div className="menu" onClick={Menu}>
                     <img src="/image/icons8-menu-24.png" alt="img" />
                 </div>
-        </nav>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" id='dropdownBtn' type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <div className="img"><img src={countryFlag} alt="" /></div>
-            </button>
-            <ul class="dropdown-menu" id='dropdownOptn'>
-            <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('US')
-                            // handleSearch()
-                        }} src="/image/united-states-of-america.png" alt="" /></div>
-                         <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('NG')
-                        }} src="/image/nigeria.png" alt="" /></div>
-                        <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('ZA')
-                        }}  src="/image/south-africa.png" alt="" /></div >
-                        <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('CA')
-                        }}  src="/image/canada.png" alt="" /></div>
-                        <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('CN')
-                        }} src="/image/china.png" alt="" /></div>
-                        <div className="img"><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('FR')
-                        }}  src="/image/france.png" alt="" /></div>
-                        <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('IN')
-                        }}  src="/image/india.png" alt="" /></div>
-                        <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('RU')
-                        }}  src="/image/russia.png" alt="" /></div>
-                        <div className="img" ><img onClick={(e)=>{
-                            changeImg(e)
-                            setcountry('JP')
-                        }}  src="/image/japan.png" alt="" /></div>
-            </ul>
-        </div>
+            </nav>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" id='dropdownBtn' type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div className="img"><img src={countryFlag} alt="" /></div>
+                </button>
+                <ul class="dropdown-menu" id='dropdownOptn'>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('US')
+                        // handleSearch()
+                    }} src="/image/united-states-of-america.png" alt="" /></div>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('NG')
+                    }} src="/image/nigeria.png" alt="" /></div>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('ZA')
+                    }} src="/image/south-africa.png" alt="" /></div >
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('CA')
+                    }} src="/image/canada.png" alt="" /></div>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('CN')
+                    }} src="/image/china.png" alt="" /></div>
+                    <div className="img"><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('FR')
+                    }} src="/image/france.png" alt="" /></div>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('IN')
+                    }} src="/image/india.png" alt="" /></div>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('RU')
+                    }} src="/image/russia.png" alt="" /></div>
+                    <div className="img" ><img onClick={(e) => {
+                        changeImg(e)
+                        setcountry('JP')
+                    }} src="/image/japan.png" alt="" /></div>
+                </ul>
+            </div>
             <div className="search">
                 <Link to='/search'>
-                <input type="text"/>
-                <button>Get news</button>
-                 </Link>
+                    <input type="text" />
+                    <button>Get news</button>
+                </Link>
             </div>
             {/* for the animation */}
             {noResult && <h1>Ops, we could not find any result</h1>}
-           {searching &&  <div className="LoadingAnimation">
+            {searching && <div className="LoadingAnimation">
                 <div className='one'>
                     <div className='two'>
                         <div className="three">
@@ -154,49 +155,48 @@ const News = () => {
                     </div>
                 </div>
             </div>}
-        <div className="newsContainer">
-           {
-            result?.map(item=>{
-     return <div className="news" key={item?.link}>
-                <a href={item?.link} target='_blank'>
-                <div className="img"> <img style={{border: "1px solid lightgray"}}  src={item?.image_url === null ? '/image/newshub.jpeg' : item?.image_url} alt="" /> </div>
-                <div className='info'>
-                    <div className="title"> <h3>{item?.title}</h3></div>
-                    <div className='wrapper'>
-                        <div className="time">
-                            <div className="img"><img src="/image/clock.png" alt="" /></div>  
-                            <p className='source'>{item?.pubDate.split(' ')[0]}</p>
+            <div className="newsContainer">
+                {
+                    result?.map(item => {
+                        return <div className="news" key={item?.link}>
+                            <a href={item?.link} target='_blank'>
+                                <div className="img"> <img style={{ border: "1px solid lightgray" }} src={item?.image_url === null ? '/image/newshub.jpeg' : item?.image_url} alt="" /> </div>
+                                <div className='info'>
+                                    <div className="title"> <h3>{item?.title}</h3></div>
+                                    <div className='wrapper'>
+                                        <div className="time">
+                                            <div className="img"><img src="/image/clock.png" alt="" /></div>
+                                            <p className='source'>{item?.pubDate.split(' ')[0]}</p>
+                                        </div>
+                                        <p className='source'>{item?.source_id}</p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                        <p className='source'>{item?.source_id}</p>
-                    </div>
-                </div>
-                </a>
+                    })
+                }
             </div>
-            })
-           }
-        </div>
-        {
-        page === null ?
-        <div></div>
-        :
-        <div>
-             {
-        pagination &&
-        <div className="pagination">
             {
-                loadMore ?
-                <div className="round"></div> 
-                :
-                <button onClick={()=>{LoadMore()}}>Load more</button>
+                page === null ?
+                    <div></div>
+                    :
+                    <div>
+                        {
+                            pagination &&
+                            <div className="pagination">
+                                {
+                                    loadMore ?
+                                        <div className="round"></div>
+                                        :
+                                        <button onClick={() => { LoadMore() }}>Load more</button>
+                                }
+                            </div>
+                        }
+                    </div>
             }
+            <Footer />
         </div>
-        
-       }
-        </div>
-       }
-      <Footer/>
-    </div>
-  )
+    )
 }
 
 export default News
